e796f87fd3a141917a410d05767873f9d7159b2f
Kevin <slash2130kevin@gmail.com>
2022-08-08 14:19:08 -0500

    Primera fase fetch inidividual funcional

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';  // useState() y useEffect() son hooks de React | useState() es para crear un estado y useEffect() es para ejecutar código cuando se renderiza el componente
 
 function App() {
+
+  const [nombre, setNombre] = useState('');
+  const [imagen, setImagen] = useState('');
+
+  useEffect(() => {  // useEffect() se ejecuta cuando se renderiza el componente
+    const url = 'https://randomuser.me/api/';
+    const peticion = fetch(url);  // fetch () es para hacer peticiones a una API
+
+    peticion  // fetch () devuelve una promesa
+      .then((response) => response.json())  // response.json() es para convertir la respuesta en un objeto
+      .then((data) => {
+        setNombre(`${data.results[0].name.first} ${data.results[0].name.last}`)
+        setImagen(data.results[0].picture.large)
+      }) // data.results[0].name.first es para acceder a la propiedad name.first del objeto data que es el que contiene la respuesta de la API (en este caso, la API devuelve un objeto con una propiedad results que es un array con un objeto que contiene la propiedad name que es un objeto con las propiedades first y last)
+      .catch((error) => console.log(error));  // catch() es para capturar errores
+  }, []);  // [] es para que solo se ejecute una vez
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Empleado del mes: </h1>
+      {nombre}
+      <div><img src={imagen} alt='IMG'></img></div>
+    </>
   );
 }
 
